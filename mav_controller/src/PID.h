e1631db07f764d0ed7fa2910d47fb6e5f23ce064
Ric92 <ricloplop@gmail.com>
2018-09-11 10:32:37 +0200

    mav_controler cleaning

--- mav_controller/src/PID.h
+++ mav_controller/src/PID.h
@@ -1,22 +1,21 @@
 //---------------------------------------------------------------------------------------------------------------------
 //  Copyright 2018 ViGUS University of Seville
 //---------------------------------------------------------------------------------------------------------------------
-//  Permission is hereby granted, free of charge, to any person obtaining a copy of this software 
-//  and associated documentation files (the "Software"), to deal in the Software without restriction, 
-//  including without limitation the rights to use, copy, modify, merge, publish, distribute, 
-//  sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is 
+//  Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+//  and associated documentation files (the "Software"), to deal in the Software without restriction,
+//  including without limitation the rights to use, copy, modify, merge, publish, distribute,
+//  sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
 //  furnished to do so, subject to the following conditions:
 //
-//  The above copyright notice and this permission notice shall be included in all copies or substantial 
+//  The above copyright notice and this permission notice shall be included in all copies or substantial
 //  portions of the Software.
 //
-//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING 
-//  BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND 
-//  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES 
-//  OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN 
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+//  BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+//  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES
+//  OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 //  CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //---------------------------------------------------------------------------------------------------------------------
-
 
 #ifndef PID_H_
 #define PID_H_
@@ -26,44 +25,82 @@
 #include <pid_tune_gui/Float32Param.h>
 #include <thread>
 
-class PID {
-public:
+class PID
+{
+  public:
     PID(float _kp, float _ki, float _kd,
         float _minSat = std::numeric_limits<float>::min(),
         float _maxSat = std::numeric_limits<float>::max(),
         float _minWind = std::numeric_limits<float>::min(),
         float _maxWind = std::numeric_limits<float>::max());
-  float update(float _val, float _incT);
- 
+    float update(float _val, float _incT);
+
     bool enableRosInterface(std::string _tag);
- 
+
     float reference() { return mReference; }
-    void reference(float _ref) { mReference = _ref; mAccumErr = 0; mLastError = 0; mLastResult = 0; mBouncingFactor = 0.1;}
- 
-    void reset(){mAccumErr = 0; mLastError = 0; mLastResult = 0; mBouncingFactor = 0.1;}
+    void reference(float _ref)
+    {
+        mReference = _ref;
+        mAccumErr = 0;
+        mLastError = 0;
+        mLastResult = 0;
+        mBouncingFactor = 0.1;
+    }
+
+    void reset()
+    {
+        mAccumErr = 0;
+        mLastError = 0;
+        mLastResult = 0;
+        mBouncingFactor = 0.1;
+    }
 
     float kp() const { return mKp; }
     float ki() const { return mKi; }
     float kd() const { return mKd; }
- 
+
     void kp(float _kp) { mKp = _kp; }
     void ki(float _ki) { mKi = _ki; }
     void kd(float _kd) { mKd = _kd; }
- 
-    void setSaturations(float _min, float _max) { mMinSat = _min; mMaxSat = _max; }
-    void getSaturations(float _min, float _max) { _min = mMinSat; _max = mMaxSat; }
- 
-    void setWindupTerms(float _min, float _max) { mWindupMin = _min; mWindupMax = _max; }
-    void getWindupTerms(float _min, float _max) { _min = mWindupMin; _max = mWindupMax; }
- 
-private:
-    bool serviceKp       (pid_tune_gui::Float32Param::Request &_req, pid_tune_gui::Float32Param::Response &_res)  {mKp = _req.param;};
-    bool serviceKi       (pid_tune_gui::Float32Param::Request &_req, pid_tune_gui::Float32Param::Response &_res)  {mKi = _req.param;};
-    bool serviceKd       (pid_tune_gui::Float32Param::Request &_req, pid_tune_gui::Float32Param::Response &_res)  {mKd = _req.param;};
-    bool serviceKsat     (pid_tune_gui::Float32Param::Request &_req, pid_tune_gui::Float32Param::Response &_res)  {mMinSat = -_req.param; mMaxSat = _req.param;};
-    bool serviceKwindup  (pid_tune_gui::Float32Param::Request &_req, pid_tune_gui::Float32Param::Response &_res)  {mWindupMin = -_req.param; mWindupMax = _req.param;};
 
-private:
+    void setSaturations(float _min, float _max)
+    {
+        mMinSat = _min;
+        mMaxSat = _max;
+    }
+    void getSaturations(float _min, float _max)
+    {
+        _min = mMinSat;
+        _max = mMaxSat;
+    }
+
+    void setWindupTerms(float _min, float _max)
+    {
+        mWindupMin = _min;
+        mWindupMax = _max;
+    }
+    void getWindupTerms(float _min, float _max)
+    {
+        _min = mWindupMin;
+        _max = mWindupMax;
+    }
+
+  private:
+    bool serviceKp(pid_tune_gui::Float32Param::Request &_req, pid_tune_gui::Float32Param::Response &_res) { mKp = _req.param; };
+    bool serviceKi(pid_tune_gui::Float32Param::Request &_req, pid_tune_gui::Float32Param::Response &_res) { mKi = _req.param; };
+    bool serviceKd(pid_tune_gui::Float32Param::Request &_req, pid_tune_gui::Float32Param::Response &_res) { mKd = _req.param; };
+    bool serviceKsat(pid_tune_gui::Float32Param::Request &_req, pid_tune_gui::Float32Param::Response &_res)
+    {
+        mMinSat = -_req.param;
+        mMaxSat = _req.param;
+    };
+    bool serviceKwindup(pid_tune_gui::Float32Param::Request &_req, pid_tune_gui::Float32Param::Response &_res)
+    {
+        mWindupMin = -_req.param;
+        mWindupMax = _req.param;
+    };
+
+  private:
     float mReference;
     float mKp, mKi, mKd;
     float mMinSat, mMaxSat;
--- mav_controller/src/main.cpp
+++ mav_controller/src/main.cpp
@@ -34,10 +34,10 @@ void Callback(const geometry_msgs::Twist &msg)
 }
 int main(int _argc, char **_argv)
 {
-	
+
 	ros::init(_argc, _argv, "publish_velocity");
 	ros::NodeHandle nh;
-	
+
 	ros::Subscriber sub1 = nh.subscribe("/pipe_pose", 1000, Callback);
 	ros::Publisher pub = nh.advertise<geometry_msgs::Twist>("/cmd_vel", 1000);
 	ros::Publisher posepub = nh.advertise<geometry_msgs::PoseStamped>("/aruco/pos", 1000);
@@ -49,7 +49,7 @@ int main(int _argc, char **_argv)
 	float i;
 	std::cout << "enter z altitude: ";
 	std::cin >> i;
-	
+
 	cv::Mat frame;
 	Mat cameraMatrix = (Mat1d(3, 3) << 726.429011, 0.000000, 283.809411, 0.000000, 721.683494, 209.109682, 0.000000, 0.000000, 1.000000);
 	Mat distCoeffs = (Mat1d(1, 5) << -0.178842, 0.660284, -0.005134, -0.005166, 0.000000);
@@ -58,18 +58,14 @@ int main(int _argc, char **_argv)
 	PID py(0.8, 0.0, 0.002, -5, 5, -20, 20);
 	PID pz(0.8, 0.0, 0.002, -5, 5, -20, 20);
 	PID gx(0.8, 0.0, 0.002, -5, 5, -20, 20);
-	
+
 	px.reference(0);
 	py.reference(0);
 	gx.reference(0);
-	
-	
+
 	px.enableRosInterface("/mav_controller/pid_x");
 	py.enableRosInterface("/mav_controller/pid_y");
 	pz.enableRosInterface("/mav_controller/pid_z");
-	//char enableRosInterface(std::string( "/aeroarms/mav_controller/pid_x");
-	//char enableRosInterface(std::string( "/aeroarms/mav_controller/pid_y");
-	//char enableRosInterface(std::string( "/aeroarms/mav_controller/pid_z");
 
 	if (i != 0)
 		pz.reference(i);
@@ -77,10 +73,10 @@ int main(int _argc, char **_argv)
 	auto t0 = chrono::steady_clock::now();
 	while (ros::ok())
 	{
-		
+
 		auto t1 = chrono::steady_clock::now();
 		auto rosTime = ros::Time::now();
-		
+
 		std::this_thread::sleep_for(std::chrono::milliseconds(10));
 		float incT = std::chrono::duration_cast<std::chrono::milliseconds>(t1 - t0).count() / 1000.0f;
 		std::cout << incT << std::endl;
@@ -89,16 +85,12 @@ int main(int _argc, char **_argv)
 		float uy = py.update(liny, incT);
 		float uz = pz.update(linz, incT);
 		float ax = gx.update(angX, incT);
-		
-
-		
 
 		geometry_msgs::Twist msg;
 		msg.linear.x = -uy;
 		msg.linear.y = ux;
 		msg.linear.z = uz;
 		msg.angular.x = ax;
-		
 
 		geometry_msgs::PoseStamped msgref;
 		msgref.header.stamp = rosTime;
@@ -106,7 +98,6 @@ int main(int _argc, char **_argv)
 		msgref.pose.position.z = pz.reference();
 		msgref.pose.position.y = py.reference();
 		msgref.pose.orientation.x = gx.reference();
-		
 
 		geometry_msgs::PoseStamped msgpos;
 		msgpos.header.stamp = rosTime;
@@ -114,13 +105,10 @@ int main(int _argc, char **_argv)
 		msgpos.pose.position.z = linz;
 		msgpos.pose.position.y = liny;
 		msgpos.pose.orientation.x = angX;
-		
 
 		pub.publish(msg);
 		posepub.publish(msgpos);
 		refpub.publish(msgref);
-
-		
 	}
 	// return (0);
 }
