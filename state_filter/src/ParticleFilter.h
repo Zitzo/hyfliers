212d73024d257b539c24d6c3aedd78ff8a1f9cb1
Ric92 <ricloplop@gmail.com>
2018-10-10 16:27:51 +0200

    Fixed rotation matrix dim

--- state_filter/src/ParticleFilter.h
+++ state_filter/src/ParticleFilter.h
@@ -59,8 +59,8 @@ class ParticleDrone : public rgbd::Particle
         cameraPosition << Xc, Yc, Zc, 1;
 
         // Transformation world-drone
-        Eigen::Affine3f rot = create_rotation_matrix(ax, ay, az, false);
-        Eigen::Matrix4f Tworld_drone = rot.matrix();
+        Eigen::Matrix4f Tworld_drone;
+        Tworld_drone = create_rotation_matrix(ax, ay, az, false);
 
         // Transformation drone-camera
         Eigen::Matrix4f Tdrone_camera;
@@ -81,8 +81,13 @@ class ParticleDrone : public rgbd::Particle
     };
 
     /// Obtain observation from world position
-    Eigen::Vector4f stateToObservation()
+    Observation stateToObservation()
     {
+        // Get orientation
+        Eigen::Vector3f ea = mOrientation.eulerAngles(0, 1, 2);
+        float ax = ea(0);
+        float ay = ea(1);
+        float az = ea(2);
 
         // Transformation camera-drone
         Eigen::Matrix3f R;
@@ -93,54 +98,63 @@ class ParticleDrone : public rgbd::Particle
         Trot.setIdentity();
         Trot.block<3, 3>(0, 0) = R;
         Eigen::Vector3f T;
-        T << 0, 0, 0;
+        T << 0, 0, 0; // Translation camera-IMU
         Eigen::Matrix4f Ttrans;
         Ttrans.setIdentity();
         Ttrans.block<3, 1>(2, 2) = T;
 
-        Eigen::Matrix4f Tcamera_drone = Trot.transpose()*(-Ttrans);
+        Eigen::Matrix4f Tcamera_drone = Trot.transpose() * (-Ttrans);
 
         //Transformation drone-world
-        //Eigen::Affine3f rot = create_rotation_matrix(ax, ay, az, true);
-        //Eigen::Matrix4f Tdrone_world = rot.matrix();
+        //Eigen::Matrix4f rot = create_rotation_matrix(ax, ay, az, true);
+        Eigen::Matrix4f Tdrone_world;
+        Tdrone_world = create_rotation_matrix(ax, ay, az, true);
 
+        Eigen::Vector4f worldPosition;
+        worldPosition << mPosition(0), mPosition(1), mPosition(2), 1;
 
+        // Camera position
+        auto cameraPosition = Tcamera_drone * Tdrone_world * worldPosition;
 
-        // float xi = mIntrinsic(0, 0)*Xc/Zc+mIntrinsic(0, 2);
-        // float yi = mIntrinsic(1, 1)*Xc/Zc+mIntrinsic(1, 2);
-        // float h = Zc / (sqrt(tan(ax) * tan(ax) + tan(ay) * tan(ay) + 1));
-        // float ax =
+        float Xc = cameraPosition(0);
+        float Yc = cameraPosition(1);
+        float Zc = cameraPosition(2);
+
+        // Observation
+        Observation z;
+        z.xi = mIntrinsic(0, 0) * Xc / Zc + mIntrinsic(0, 2);
+        z.yi = mIntrinsic(1, 1) * Xc / Zc + mIntrinsic(1, 2);
+        z.altitude = Zc / (sqrt(tan(ax) * tan(ax) + tan(ay) * tan(ay) + 1));
+        return z;
     };
 
     // Get rotation matrix from roll, pitch and yaw
-    Eigen::Affine3f create_rotation_matrix(float ax, float ay, float az, bool inverse)
+    Eigen::Matrix4f create_rotation_matrix(const float ax_,const float ay_,const float az_, bool inverse)
     {
-        Eigen::Affine3f rx = Eigen::Affine3f(Eigen::AngleAxisf(ax, Eigen::Vector3f(1, 0, 0)));
-        Eigen::Affine3f ry = Eigen::Affine3f(Eigen::AngleAxisf(ay, Eigen::Vector3f(0, 1, 0)));
-        Eigen::Affine3f rz = Eigen::Affine3f(Eigen::AngleAxisf(az, Eigen::Vector3f(0, 0, 1)));
+        Eigen::Matrix3f rx,ry,rz;
+        rx = Eigen::AngleAxisf(ax_ * M_PI, Eigen::Vector3f::UnitX());
+        ry = Eigen::AngleAxisf(ay_ * M_PI, Eigen::Vector3f::UnitY());
+        rz = Eigen::AngleAxisf(az_ * M_PI, Eigen::Vector3f::UnitZ());
+        
+        Eigen::Matrix4f rotMatrix;
+        rotMatrix.setIdentity();
         if (inverse)
         {
-            return rz.transpose() * ry.transpose() * rx.transpose();
+            Eigen::Matrix3f rotationMatrix = rz.transpose() * ry.transpose() * rx.transpose();
+            rotMatrix.block<3, 3>(0, 0) = rotationMatrix;
         }
         else
         {
-            return rz * ry * rx;
+            Eigen::Matrix3f rotationMatrix = rz * ry * rx;
+            rotMatrix.block<3, 3>(0, 0) = rotationMatrix;
         }
+        return rotMatrix;
     }
 
   private:
     Eigen::Vector3f mPosition;
     Eigen::Matrix3f mOrientation;
     Eigen::Matrix<float, 3, 3> mIntrinsic;
-};
-
-struct Observation
-{
-    float altitude;
-    float xi;
-    float yi;
-    Eigen::Quaternionf quat;
-    std::chrono::steady_clock::time_point time;
 };
 
 template <typename ParticleType_>
